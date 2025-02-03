b3830a4f30cc74540c4770ec66b396fe4a7a69e4
Wesley <wesleyisrael2013@hotmail.com>
2025-02-03 10:56:55 +0000

    Updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className='App-header'>
-        <h1>Photo Viewer</h1>
         <PhotoViewer />
       </header>
     </div>
--- src/PhotoViewer.js
+++ src/PhotoViewer.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import 'react-image-lightbox/style.css';
 import Lightbox from 'react-image-lightbox';
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
 
 const PhotoViewer = () => {
   const [images, setImages] = useState([]);
@@ -30,21 +33,42 @@ const PhotoViewer = () => {
     setCurrentIndex((currentIndex + 1) % images.length);
   };
 
+  const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
+
   return (
     <div>
-      <input 
-        type="file" 
-        accept="image/*" 
-        multiple 
-        onChange={handleFileChange} 
-      />
+      <IconButton
+        size="large"
+        color="inherit"
+        component="label"
+        role={undefined}
+        variant="contained"
+        tabIndex={-1}
+      >
+        <AddAPhotoIcon fontSize='large' />
+        <VisuallyHiddenInput
+          type="file"
+          onChange={handleFileChange}
+          multiple
+        />
+      </IconButton>
       <div style={{ display: 'flex', flexWrap: 'wrap', marginTop: '20px' }}>
         {images.map((image, index) => (
           <div key={index} style={{ margin: '10px' }}>
-            <img 
-              src={image} 
-              alt={`Uploaded ${index}`} 
-              style={{ width: '150px', height: '150px', objectFit: 'cover', cursor: 'pointer' }} 
+            <img
+              src={image}
+              alt={`Uploaded ${index}`}
+              style={{ width: '235px', height: '235px', objectFit: 'cover', cursor: 'pointer' }}
               onClick={() => openLightbox(index)}
             />
           </div>
